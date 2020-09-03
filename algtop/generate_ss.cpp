53b666c74ce1dde863376f8cf34af2250315cca4
Weinan Lin <lwnpku2015@gmail.com>
2020-09-03 16:55:58 -0500

    Refactor using std::map

--- algtop/generate_ss.cpp
+++ algtop/generate_ss.cpp
@@ -2,147 +2,105 @@
 
 struct BasisSS
 {
-	array base_indices;
-	array diff_indices;
-	int level;
-	int s;
-	int t;
-	int v;
-
-	BasisSS(array&& base_indices_, array&& diff_indices_, int level_, int s_, int t_, int v_) :
-		base_indices(base_indices_), diff_indices(diff_indices_), level(level_), s(s_), t(t_), v(v_) {};
-	BasisSS() : level(-1), s(-1), t(-1), v(-1) {};
+	array2d basis_ind;
+	array2d diffs_ind;
+	array levels;
 };
 
-struct DgaBasisMon
-{
-	array diff_indices;
-	int s;
-	int t;
-	int v;
-
-	DgaBasisMon(array&& diff_indices_, int s_, int t_, int v_) :
-		diff_indices(diff_indices_), s(s_), t(t_), v(v_) {};
-};
-
-void load_basis(sqlite3* conn, const std::string& table_name, std::vector<DgaBasisMon>& basis)
+void load_mon_diffs(sqlite3* conn, const std::string& table_name, std::map<Deg, array2d>& diffs)
 {
 	sqlite3_stmt* stmt;
 	std::string cmd = std::string("SELECT diff, s, t, v FROM ") + table_name + " ;";
 	sqlite3_prepare_v100(conn, cmd, &stmt);
 	while (sqlite3_step(stmt) == SQLITE_ROW) {
-		basis.emplace_back(str_to_array(sqlite3_column_str(stmt, 0)),
-			sqlite3_column_int(stmt, 1), sqlite3_column_int(stmt, 2), sqlite3_column_int(stmt, 3));
+		const Deg deg = { sqlite3_column_int(stmt, 1), sqlite3_column_int(stmt, 2), sqlite3_column_int(stmt, 3) };
+		diffs[deg].push_back(str_to_array(sqlite3_column_str(stmt, 0)));
 	}
 	sqlite3_finalize(stmt);
 }
 
-bool cmp_ss(const BasisSS& b, const std::array<int, 3>& value)
-{
-	if (b.t < value[1])
-		return true;
-	else if (b.t == value[1])
-		if (b.s < value[0])
-			return true;
-		else if (b.s == value[0])
-			if (b.v < value[2])
-				return true;
-	return false;
-}
-
-void generate_ss(sqlite3* conn, const std::string& table_name_basis, const char* table_ss, int r)
+void generate_ss(sqlite3* conn, const std::string& table_name_basis, const std::string& table_ss, int r)
 {
 	/* Compile SQL statements */
 	sqlite3_stmt* stmt_update_basis;
 	std::string cmd_update_basis = std::string("INSERT INTO ") + table_ss + " (base, diff, level, s, t, v) VALUES (?1, ?2, ?3, ?4, ?5, ?6);";
 	sqlite3_prepare_v100(conn, cmd_update_basis, &stmt_update_basis);
 
-	std::vector<std::array<int, 5>> indices_tsv;
-	std::string table_indices_tsv = std::string(table_name_basis) + "_indices_stv";
-	//load_mon_indices_tsv(conn, table_indices_tsv, indices_tsv);
-	std::cout << "num_mons_tsv loaded! Size=" << indices_tsv.size() << '\n';
+	execute_cmd(conn, "DELETE FROM " + table_ss + ";");
 
-	std::vector<DgaBasisMon> basis;
-	load_basis(conn, table_name_basis, basis);
-	std::cout << "basis loaded! Size=" << basis.size() << '\n';
+	std::map<Deg, array2d> mon_diffs;
+	load_mon_diffs(conn, table_name_basis, mon_diffs);
+	std::cout << "mon_diffs loaded! Size=" << mon_diffs.size() << '\n';
 
-	std::vector<BasisSS> basis_ss;
-	basis_ss.resize(basis.size());
+	std::map<Deg, BasisSS> basis_ss;
 
 	/* fill basis_ss */
 	int prev_t = 0;
-	for (const auto& index : indices_tsv) {
-		const int &t = index[0], &s = index[1], &v = index[2], &a = index[3], &b = index[4];
-		if (t > prev_t) {
-			prev_t = t;
-			std::cout << "t=" << t << '\n';
+	for (auto& [deg, mon_diffs_d] : mon_diffs) {
+		if (deg.t > prev_t) {
+			prev_t = deg.t;
+			std::cout << "Compute basis_ss, t=" << deg.t << "          \r";
 		}
+		BasisSS& basis_ss_d = basis_ss[deg];
 
-		array range_ = range(0, b - a);
+		array range_ = range((int)mon_diffs_d.size());
 
-		int i;
 		array lead_image;
-		for (i = a; i < b; ++i) {
-			if (basis_ss[i].base_indices.empty())
-				break;
-			lead_image.push_back(basis_ss[i].base_indices[0]);
-		}
+		for (const auto& base : basis_ss_d.basis_ind)
+			lead_image.push_back(base.front());
 		std::sort(lead_image.begin(), lead_image.end());
+		if (!lead_image.empty() && (range_.empty() || lead_image.back() > range_.back()))
+			std::cout << "test\n";
 
-		/*if (!lead_image.empty() && lead_image.back() >= b - a) {
-			std::cout << "range_=" << range_ << '\n';
-			std::cout << "lead_image=" << lead_image << '\n';
-			std::cout << "test: " << t << " " << s << " " << v << " " << a << " " << b << '\n';
-		}*/
 		array x = add_vectors(range_, lead_image);
 
 		array2d fx;
 		for (int xi : x)
-			fx.push_back(basis[size_t(a) + xi].diff_indices);
+			fx.push_back(mon_diffs_d[xi]);
 
 		array2d image, kernel, g;
 		set_linear_map(x, fx, image, kernel, g);
 
-		/* fill with the kernel after the image */
+		/* fill with other cycles after the boundaries */
 		array lead_kernel;
-		for (size_t j = 0; j < kernel.size(); j++) {
-			lead_kernel.push_back(kernel[j][0]);
-			basis_ss[i + j] = BasisSS(std::move(kernel[j]), array(), T_MAX - r, s, t, v);
+		for (auto& cycle : kernel) {
+			lead_kernel.push_back(cycle.front());
+			basis_ss_d.basis_ind.push_back(cycle);
+			basis_ss_d.diffs_ind.push_back({});
+			basis_ss_d.levels.push_back(T_MAX - r);
 		}
 		std::sort(lead_kernel.begin(), lead_kernel.end());
 
-		/* fill with the image */
-		auto ab_image = get_ab(indices_tsv, s + 1, t, v - r);
-		for (size_t k = 0; k < image.size(); k++)
-			basis_ss[ab_image.first + k] = BasisSS(std::move(image[k]), array(), r, s + 1, t, v - r);
+		/* fill with boundaries */
+		Deg deg_diff = deg + Deg{ 1, 0, -r };
+		for (auto& boundary : image) {
+			basis_ss[deg_diff].basis_ind.push_back(std::move(boundary));
+			basis_ss[deg_diff].diffs_ind.push_back({});
+			basis_ss[deg_diff].levels.push_back(r);
+		}
 
 		/* fill with the rest */
-		array rest;
-		std::set_symmetric_difference(x.begin(), x.end(), lead_kernel.begin(), lead_kernel.end(), std::back_inserter(rest));
-		for (size_t j = 0; j < rest.size(); j++) {
-			basis_ss[i + kernel.size() + j] = BasisSS({ rest[j] }, std::move(basis[size_t(a) + rest[j]].diff_indices), T_MAX, s, t, v);
+		array rest = add_vectors(x, lead_kernel);
+		for (int i : rest) {
+			basis_ss_d.basis_ind.push_back({ i });
+			basis_ss_d.diffs_ind.push_back(std::move(mon_diffs[deg][i]));
+			basis_ss_d.levels.push_back(T_MAX);
 		}
 	}
 
 	/* Insert into the database */
 	execute_cmd(conn, "BEGIN TRANSACTION");
-	prev_t = 0;
-	for (const auto& b_ss : basis_ss) {
-		if (b_ss.t > prev_t) {
-			prev_t = b_ss.t;
-			std::cout << "t=" << b_ss.t << '\n';
-			execute_cmd(conn, "END TRANSACTION");
-			execute_cmd(conn, "BEGIN TRANSACTION");
+	for (const auto& [deg, basis_ss_d] : basis_ss) {
+		for (size_t i = 0; i < basis_ss_d.basis_ind.size(); ++i) {
+			sqlite3_bind_str(stmt_update_basis, 1, array_to_str(basis_ss_d.basis_ind[i]));
+			sqlite3_bind_str(stmt_update_basis, 2, array_to_str(basis_ss_d.diffs_ind[i]));
+			sqlite3_bind_int(stmt_update_basis, 3, basis_ss_d.levels[i]);
+			sqlite3_bind_int(stmt_update_basis, 4, deg.s);
+			sqlite3_bind_int(stmt_update_basis, 5, deg.t);
+			sqlite3_bind_int(stmt_update_basis, 6, deg.v);
+			sqlite3_step(stmt_update_basis);
+			sqlite3_reset(stmt_update_basis);
 		}
-		sqlite3_bind_str(stmt_update_basis, 1, array_to_str(b_ss.base_indices));
-		sqlite3_bind_str(stmt_update_basis, 2, array_to_str(b_ss.diff_indices));
-		sqlite3_bind_int(stmt_update_basis, 3, b_ss.level);
-		sqlite3_bind_int(stmt_update_basis, 4, b_ss.s);
-		sqlite3_bind_int(stmt_update_basis, 5, b_ss.t);
-		sqlite3_bind_int(stmt_update_basis, 6, b_ss.v);
-		sqlite3_step(stmt_update_basis);
-		sqlite3_reset(stmt_update_basis);
-		//std::cout << b_ss.base_indices << " " << b_ss.diff_indices << " " << b_ss.level << " " << b_ss.s << " " << b_ss.t << " " << b_ss.v << '\n';
 	}
 	execute_cmd(conn, "END TRANSACTION");
 
@@ -157,8 +115,8 @@ int main_generate_ss(int argc, char** argv)
 	const char* table_basis, * table_ss;
 	int t_max;
 	if (argc == 1) {
-		table_basis = "E2t_basis";
-		table_ss = "E2t_ss";
+		table_basis = "E2_basis";
+		table_ss = "E2_ss";
 		t_max = 74;
 	}
 	else {
